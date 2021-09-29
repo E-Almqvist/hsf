fb64476b31bfa873d24d41002475e9933e003c6a
E. Almqvist <elalmqvist@gmail.com>
2021-09-29 11:01:31 +0200

    Stuff

---
+++ wesweb01/webbserver_test/app.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/ruby 
+
+require "sinatra"
+
+def read_page(page)
+	begin
+		content = File.read("public/#{page}.html")
+		return content
+	rescue Exception => err
+		return ":("
+	end
+end
+
+get("/") do
+	return "Hello world!"
+end
+
+get("/page/:page") do
+	page = params[:page]
+	page = page.gsub(".", "")
+	content = read_page(page).gsub("$TITLE", page.upcase())
+
+	return content
+end
+
+get("/fruits") do
+	fruits = %w(Apple Orange Banana Grillkorv Varmkorv Frukt Hej)
+
+	listitems = ""
+	fruits.each do |fruit|
+		listitems += "<li>#{fruit}</li>"
+	end
+
+	response = "<ul>#{listitems}</ul>"
+	return response
+end
