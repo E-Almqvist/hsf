3fd9f500c4c41fe34ff3899642857b20caa5f0ae
E. Almqvist <elalmqvist@gmail.com>
2021-10-27 11:54:46 +0200

    Login stuff

--- wesweb01/evil_infosite/app.rb
+++ wesweb01/evil_infosite/app.rb
@@ -5,20 +5,49 @@ require "slim"
 
 load "db_parse.rb"
 
+enable :sessions
+
 db_cache = get_db() # get all the parsed data
+
+user_creds = {
+	admin: "superpassword"
+}
 
 get "/" do
 	slim(:person, locals: {persondata: db_cache})
 end
 
-get "/login" do
+get "/loginpage" do
 	slim(:login)
 end
 
-get "/userpage" do
-	userinfo = {
-		username: params[:username],
-		password: params[:password]
-	}
-	slim(:userpage, locals: {userinfo: userinfo})
+get "/registerpage" do
+	slim(:register)
+end
+
+post "/register" do
+	username = params[:username].to_sym
+	if( user_creds[username] == nil ) then
+		user_creds[username] = params[:password]
+		session[:msg] = "Account created."
+		redirect "/loginpage"
+	else
+		session[:msg] = "Username in use."
+		redirect "/loginpage"
+	end
+
+end
+
+post "/login" do
+	username = params[:username]
+	password = params[:password]
+
+	if( user_creds[username.to_sym] == password ) then
+		session[:username] = username
+		session[:msg] = "Login success."
+		redirect "/"
+	else
+		session[:msg] = "Login failed."
+		redirect "/login"
+	end
 end
