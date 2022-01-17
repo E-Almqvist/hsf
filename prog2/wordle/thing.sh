df74e1f6930e75d1917124b8c3db7e426d7654a6
E. Almqvist <elalmqvist@gmail.com>
2022-01-17 10:08:42 +0100

    More stuff

--- prog2/wordle/thing.sh
+++ prog2/wordle/thing.sh
@@ -5,4 +5,5 @@ words=/usr/share/dict/words
 thing=$(ruby calc.rb $1 $2)
 
 echo $thing
-cat $words | awk '{print tolower($0)}' | grep -E '${thing}(\w+)'
+cat $words | awk '{print tolower($0)}' 
+#| grep -E '${thing}(\w+)'
