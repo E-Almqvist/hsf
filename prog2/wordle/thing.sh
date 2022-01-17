6328be52f7bd364d270df0499c311979987966bd
E. Almqvist <elalmqvist@gmail.com>
2022-01-17 14:09:43 +0100

    Bug fix

--- prog2/wordle/thing.sh
+++ prog2/wordle/thing.sh
@@ -3,9 +3,6 @@
 words=/usr/share/dict/words
 
 thing=$(ruby calc.rb $1 $2)
-
-echo $thing
 fivewords=$(cat $words | awk '{print tolower($0)}' | grep -E '^[[:alpha:]]{5}$')
 
-echo $fivewords
-#| grep -E '\b${thing}'
+echo $fivewords | sed 's/ /\n/g' | grep -E "^${thing}"
